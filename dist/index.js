fa0c64878e2d87d280f7332e9d318698f283ee1f
nxvtej <nxvtej@outlook.com>
2024-12-02 16:40:36 +0530

    basic working

--- dist/index.js
+++ dist/index.js
@@ -6,13 +6,23 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const http_1 = __importDefault(require("http"));
 const express_1 = __importDefault(require("express"));
 const path_1 = __importDefault(require("path"));
-const port = process.env.PORT || 3000;
+const socket_io_1 = require("socket.io");
+const port = process.env.PORT || 4000;
 const app = (0, express_1.default)();
 const server = http_1.default.createServer(app);
-// key thing, i cant directly do app.listen() because i need to pass the server to socket.io thus i need http server
+const io = new socket_io_1.Server(server);
 const filePath = path_1.default.resolve(__dirname, '../src/public');
+// key thing, i cant directly do app.listen() because i need to pass the server to socket.io thus i need http server
 app.use(express_1.default.static(filePath));
 app.get('/', (req, res) => {
     res.sendFile(path_1.default.join(filePath, 'index.html'));
+});
+io.on('connection', (client) => {
+    console.log('a user connected', client.id);
+    client.on('message', (message) => {
+        console.log('message: ' + message);
+        // koi frnotend se aya to sabko bhej dega
+        io.emit('message', message);
+    });
 });
 server.listen(port, () => { console.log(`Server is running on port ${port}`); });
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,30 @@
 import http from 'http';
 import express from 'express';
 import path from 'path';
+import { Server } from 'socket.io';
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 const app = express();
 const server = http.createServer(app);
-
-// key thing, i cant directly do app.listen() because i need to pass the server to socket.io thus i need http server
-
+const io = new Server(server);
 const filePath = path.resolve(__dirname, '../src/public');
+// key thing, i cant directly do app.listen() because i need to pass the server to socket.io thus i need http server
 
 app.use(express.static(filePath));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(filePath, 'index.html'));
 });
+
+io.on('connection', (client) => {
+    console.log('a user connected', client.id);
+
+    client.on('message', (message) => {
+        console.log('message: ' + message);
+        // koi frnotend se aya to sabko bhej dega
+        io.emit('message', message);
+    });
+
+});
+
 server.listen(port, () => { console.log(`Server is running on port ${port}`) });
